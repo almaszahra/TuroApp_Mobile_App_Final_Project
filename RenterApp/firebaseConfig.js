68c71775223aa61bff4095de97081c1db7defb71
azahra <almasziaya@gmail.com>
2025-04-13 03:51:36 -0400

    Added firebaseConfig.js to make sure both apps connect to the same database

---
+++ RenterApp/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC63ukAVPMkt3PWUDDF0weiwbKLKCpCMLY",
+  authDomain: "turoappmobileappfinalproject.firebaseapp.com",
+  projectId: "turoappmobileappfinalproject",
+  storageBucket: "turoappmobileappfinalproject.firebasestorage.app",
+  messagingSenderId: "442021809372",
+  appId: "1:442021809372:web:b674c9f7b652218b04b819"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
